d10b64e07d2a5dcd802a1d5a04a18799ea61853a
Alex Moriyama <alex.moriyama@gmail.com>
2023-04-11 16:29:52 -0700

    updates post-tutor session

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
-var APIkey = "5ee75c83df750ef29f91c4939410e160";
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// var APIkey = "5ee75c83df750ef29f91c4939410e160";
+var city = document.querySelector("#get-city");
+var startBtn = document.querySelector("#get-weather-data");
+var dayContainer = document.querySelector("#day-container");
+var futureContainer = document.querySelector("future-container");
+var currentDay = dayjs().format('DD M YYYY')
+// var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIkey
+
+
+
+function cityName(event){
+    event.preventDefault()
+    var cityValue = city.value
+    getWeatherApi(cityValue)
+    // Call the function to create the history
+    console.log("my-city", cityValue)
+}
+
+function getWeatherApi(city){
+    var APIkey = "5ee75c83df750ef29f91c4939410e160";
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIkey
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+     .then (function(data) {
+        console.log(data);
+        console.log(currentDay);
+        var nameOfCity = document.createElement("h3");
+        var tempOfCity = document.createElement("h3");
+        var windOfCity = document.createElement("h3");
+        var humidityOfCity = document.createElement("h3");
+        var iconValue = data.weather[0].icon;
+        var icon = "http://openweathermap.org/img/wn/" + iconValue + ".png"
+        var cityLattitude = data.coord.lat;
+        var cityLongitutde = data.coord.lon;
+
+        nameOfCity.textContent = data.name + " " + currentDay
+        tempOfCity.textContent = Math.round((data.main.temp - 273.15) * 9 / 5 + 32) + "\xB0" + "F";
+        dayContainer.append(nameOfCity);
+        dayContainer.append(iconValue);
+        dayContainer.append(tempOfCity);
+
+     })
+}
+
+function getWeatherHistory(){
+    var citySearch = city.value
+    var localStorage = JSON.parse(localStorage.getItem("searched-city"))
+    localStorage.push(citySearch)
+    localStorage.setItem("searched-city", JSON.stringify(localStorage))
+}
+
+startBtn.addEventListener("click", cityName)
